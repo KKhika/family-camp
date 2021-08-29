3559a88753f136ad28e9307731104ea18dbe92b3
KKhika <64680331+KKhika@users.noreply.github.com>
2021-08-29 14:22:39 +0900

    Add files via upload

--- family-camp/js/camp-style.js
+++ family-camp/js/camp-style.js
@@ -6,13 +6,13 @@ $(function(){
         img__mountain.removeClass('hidden');
         i = i + 1;
         if(i%3 == 0) {
-            $('.camp-style').text('キャンプスタイル-Solo');
+            $('.camp-style').html('<span>Solo</span>（ソロキャンプ）');
             img__mountain.html('<img src="images/image_02_solo.jpg" alt=""></img>');
         }else if(i%3 ==1) {
-            $('.camp-style').text('キャンプスタイル-Duo');
+            $('.camp-style').html('<span>Duo</span>（デュオキャンプ）');
             img__mountain.html('<img src="images/image_03_couple.png" alt=""></img>');
         }else if(i%3 ==2)  {
-            $('.camp-style').text('キャンプスタイル-Family');
+            $('.camp-style').html('<span>Family</span>（ファミリーキャンプ）');
             img__mountain.html('<img src="images/image_06_family.jpg" alt=""></img>');
         }
         
@@ -26,13 +26,13 @@ $(function(){
         img__design.removeClass('hidden');
         j = j + 1;
         if(j%3 == 0) {
-            $('.camp-field').text('キャンプ場-熊本');
+            $('.camp-field').html('<span>熊本</span>');
             img__design.html('<img src="images/image_01_kumamoto.jpg" alt=""></img>');
         }else if(j%3 == 1) {
-            $('.camp-field').text('キャンプ場-佐賀');
+            $('.camp-field').html('<span>佐賀</span>');
             img__design.html('<img src="images/image_04_saga.jpg" alt=""></img>');
         }else if(j%3 == 2) {
-            $('.camp-field').text('キャンプ場-長崎');
+            $('.camp-field').html('<span>長崎</span>');
             img__design.html('<img src="images/image_05_nagasaki.jpg" alt=""></img>');
         }
         
